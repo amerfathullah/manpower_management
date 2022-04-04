b1fb79ac8aaa464356cb694745d5f967f1f8134b
amerfathullah <amerfathullah@rocketmail.com>
2022-04-04 10:01:55 +0800

    Patient: empty table

--- lib/models/patient.dart
+++ lib/models/patient.dart
@@ -23,30 +23,30 @@ class Patient {
   });
 
   static List<Patient> patients = [
-    Patient(
-      id: '0',
-      name: 'Amer Fathullah',
-      shift: Shift.A,
-      startDate: DateTime.now(),
-      endDate: DateTime.now().add(const Duration(days: 6)),
-      station: 'Main Station',
-    ),
-    Patient(
-      id: '1',
-      name: 'Fazli Jamaluddin',
-      shift: Shift.B,
-      startDate: DateTime.now().add(const Duration(days: 1)),
-      endDate: DateTime.now().add(const Duration(days: 7)),
-      station: 'Station A',
-    ),
-    Patient(
-      id: '2',
-      name: 'Abdullah Hadi',
-      shift: Shift.C,
-      startDate: DateTime.now().add(const Duration(days: 2)),
-      endDate: DateTime.now().add(const Duration(days: 8)),
-      station: 'Main Station',
-    ),
+    // Patient(
+    //   id: '0',
+    //   name: 'Amer Fathullah',
+    //   shift: Shift.A,
+    //   startDate: DateTime.now(),
+    //   endDate: DateTime.now().add(const Duration(days: 6)),
+    //   station: 'Main Station',
+    // ),
+    // Patient(
+    //   id: '1',
+    //   name: 'Fazli Jamaluddin',
+    //   shift: Shift.B,
+    //   startDate: DateTime.now().add(const Duration(days: 1)),
+    //   endDate: DateTime.now().add(const Duration(days: 7)),
+    //   station: 'Station A',
+    // ),
+    // Patient(
+    //   id: '2',
+    //   name: 'Abdullah Hadi',
+    //   shift: Shift.C,
+    //   startDate: DateTime.now().add(const Duration(days: 2)),
+    //   endDate: DateTime.now().add(const Duration(days: 8)),
+    //   station: 'Main Station',
+    // ),
   ];
 
   List<Patient> get patient {
